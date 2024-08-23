e2c54aea6dffdce105caf1731ee9540eccf16d5d
Poisonlocket <loribono@gmx.ch>
2024-08-23 22:22:42 +0200

    initialized tailwindcss and daisyui

---
+++ frontend/LEFTOVERLove/tailwind.config.js
@@ -0,0 +1,11 @@
+import daisyui from "daisyui";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{vue,js,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require(daisyui)],
+}
+
