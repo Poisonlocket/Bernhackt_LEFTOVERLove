95e2b2e783d9d6ce6a75f78e560446d8327fc21a
githubUser3454321 <116742137+githubUser3454321@users.noreply.github.com>
2024-08-24 15:16:23 +0200

    Reapply "Updated location to double"
    This reverts commit 672b7cd912114b7a32145018babe8e9c2b513a0f.

--- Code/backend/LeftOverLove.Common/Entities/Customer.cs
+++ Code/backend/LeftOverLove.Common/Entities/Customer.cs
@@ -5,7 +5,7 @@ public class Customer
     public int Id { get; set; }
     public string FirstName { get; set; } = null!;
     public string LastName { get; set; } = null!;
-    public string Longitude { get; set; } = null!;
-    public string Latitude { get; set; } = null!;
+    public double Longitude { get; set; } 
+    public double Latitude { get; set; }
     public int Points { get; set; }
 }
