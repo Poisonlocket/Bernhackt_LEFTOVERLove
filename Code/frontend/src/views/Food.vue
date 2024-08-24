4e1d0ac943e9809a0d3699c69fd25bbf215f5f67
Christian Huber <corethereal@gmail.com>
2024-08-24 18:51:52 +0200

    Map + Modal, Fixes

--- Code/frontend/src/views/Food.vue
+++ Code/frontend/src/views/Food.vue
@@ -2,13 +2,13 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Foods</ion-title>
+        <ion-title>Food</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title color="primary" size="large">Foods</ion-title>
+          <ion-title color="primary" size="large">Food</ion-title>
           <ion-subtitle class="p-4 text-xl text-neutral-400">Check available food items</ion-subtitle>
         </ion-toolbar>
       </ion-header>
--- Code/frontend/src/views/TabsPage.vue
+++ Code/frontend/src/views/TabsPage.vue
@@ -2,26 +2,23 @@
   <ion-page>
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
-      <!-- <ion-content>
-        <ion-button class="p-4" @click="console.log('Hello')" expand="block">Click me</ion-button>
-      </ion-content> -->
       <ion-tab-bar class="py-2" slot="bottom">
         <ion-tab-button tab="map" href="/tabs/map">
           <ion-icon aria-hidden="true" :icon="mapOutline" />
           <ion-label>Map</ion-label>
         </ion-tab-button>
         
-        <ion-tab-button tab="foods" href="/tabs/foods">
-          <ion-icon aria-hidden="true" :icon="pizzaOutline" />
-          <ion-label>Foods</ion-label>
+        <ion-tab-button tab="food" href="/tabs/food">
+          <ion-icon aria-hidden="true" :icon="restaurantOutline" />
+          <ion-label>Food</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab3" href="/tabs/messages">
+        <ion-tab-button tab="messages" href="/tabs/messages">
           <ion-icon aria-hidden="true" :icon="chatboxOutline" />
           <ion-label>Messages</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab4" href="/tabs/user">
+        <ion-tab-button tab="user" href="/tabs/user">
           <ion-icon aria-hidden="true" :icon="personCircleOutline" />
           <ion-label>User</ion-label>
         </ion-tab-button>
@@ -32,5 +29,5 @@
 
 <script setup lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { pizzaOutline, mapOutline, chatboxOutline, personCircleOutline } from 'ionicons/icons';
+import { restaurantOutline, mapOutline, chatboxOutline, personCircleOutline } from 'ionicons/icons';
 </script>
